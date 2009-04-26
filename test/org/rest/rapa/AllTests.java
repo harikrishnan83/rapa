da63bd00b1a717f1be616fad2c05da9b373136ae
harikrishnan83 <harikrishnan83@412cab6e-e6cb-11dd-97d9-e7a3ff7b18fb>
2009-04-26 07:05:03 +0000

    Commiting in XMLHandlerTest

--- test/org/rest/rapa/AllTests.java
+++ test/org/rest/rapa/AllTests.java
@@ -6,10 +6,11 @@ import junit.framework.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.rest.rapa.formatter.JSonHandlerTest;
+import org.rest.rapa.formatter.XMLHandlerTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses( { RestClientCoreTest.class, HttpMethodExcecutorTest.class,
-		FormatHandlerFactoryTest.class, JSonHandlerTest.class })
+		FormatHandlerFactoryTest.class, JSonHandlerTest.class, XMLHandlerTest.class })
 public class AllTests {
 	public static Test suite() {
 		return new JUnit4TestAdapter(AllTests.class);
