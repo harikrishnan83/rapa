8f8ca55edeb3d580222aca89e7f193addbb026d8
harikrishnan83 <harikrishnan83@412cab6e-e6cb-11dd-97d9-e7a3ff7b18fb>
2009-04-30 15:36:04 +0000

    Eclipse maven

---
+++ src/main/java/org/rest/rapa/formatter/XMLHandler.java
@@ -0,0 +1,31 @@
+package org.rest.rapa.formatter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.StringReader;
+
+import javax.xml.bind.JAXB;
+
+import org.rest.rapa.resource.Resource;
+
+public class XMLHandler implements FormatHandler {
+
+	public Resource deserialize(String content, Class clazz) {
+		return (Resource) JAXB.unmarshal(new StringReader(content), clazz);
+	}
+
+	public String serialize(Resource resource) {
+		OutputStream outputStream = new ByteArrayOutputStream();
+		JAXB.marshal(resource, outputStream);
+		return outputStream.toString();
+	}
+
+	public String getExtension() {
+		return "xml";
+	}
+
+	public String getContentType() {
+		return "text/xml";
+	}
+
+}
