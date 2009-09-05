feb38357f959b0415ac7567e13aab7e8cbe337fe
s.sai.venkat <s.sai.venkat@412cab6e-e6cb-11dd-97d9-e7a3ff7b18fb>
2009-09-05 12:44:45 +0000

    Added a rest client builder as a facade to RestClient

---
+++ src/main/java/org/rest/rapa/RestClientBuilder.java
@@ -0,0 +1,66 @@
+package org.rest.rapa;
+
+import org.rest.rapa.formatter.Formats;
+
+public class RestClientBuilder {
+	private String url;
+	private String password;
+	private String username;
+	private String host;
+	private int port;
+	private String scheme;
+	private String realm;
+	private Formats format;
+	private boolean formatAsExtenstion;
+	
+	public RestClientBuilder withUrl(String url){
+		this.url = url;
+		return this;
+	}
+	public RestClientBuilder withUsername(String username){
+		this.username = username;
+		return this;
+	}
+	public RestClientBuilder withPassword(String password){
+		this.password = password;
+		return this;
+	}
+	public RestClientBuilder withHost(String host){
+		this.host = host;
+		return this;
+	}
+	
+	public RestClientBuilder withPort(int port){
+		this.port = port;
+		return this;
+	}
+	
+	public RestClientBuilder withScheme(String scheme){
+		this.scheme = scheme;
+		return this;
+	}
+	
+	public RestClientBuilder withRealm(String realm){
+		this.realm = realm;
+		return this;
+	}
+	
+	public RestClientBuilder withFormat(Formats format){
+		this.format = format;
+		return this;
+	}
+	
+	public RestClientBuilder useFormatAsExtension(){
+		this.formatAsExtenstion = true;
+		return this;
+	}
+
+	public RestClientBuilder donotUseFormatAsExtension(){
+		this.formatAsExtenstion = false;
+		return this;
+	}
+	
+	public RestClient build(){
+		return new RestClient(url, username, password, host, port, scheme, realm, format, formatAsExtenstion);
+	}
+}
