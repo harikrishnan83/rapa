5939ff648c95975aedf1d2fcf2679a1f5c58caa3
unknown <harikri@.corporate.thoughtworks.com>
2009-10-07 21:12:47 +0530

    checking in Failing test

--- test/org/rest/rapa/UrlTest.java
+++ test/org/rest/rapa/UrlTest.java
@@ -5,10 +5,10 @@ import java.net.MalformedURLException;
 import org.junit.Test;
 
 public class UrlTest {
-	
+
 	private static final int ID = 1;
 	private static final String EXTENSION = "xml";
-	private static final String BASEURL = "http://www.test.com";
+	private static final String BASEURL = "http://www.tst.com";
 	private Url url;
 
 	@Test
@@ -16,13 +16,13 @@ public class UrlTest {
 		url = new Url(BASEURL, EXTENSION, true);
 		assertEquals(BASEURL + "/" + ID + "." + EXTENSION, url.getResourceSpecificURL(ID));
 	}
-	
+
 	@Test
 	public void shouldReturnResourceSpecificUrlWithOutExtension() {
 		url = new Url(BASEURL, EXTENSION, false);
 		assertEquals(BASEURL + "/" + ID, url.getResourceSpecificURL(ID));
 	}
-	
+
 	@Test
 	public void shouldReturnUrlWithExtension() {
 		url = new Url(BASEURL, EXTENSION, true);
@@ -34,23 +34,23 @@ public class UrlTest {
 		url = new Url(BASEURL, EXTENSION, false);
 		assertEquals(BASEURL, url.getURL());
 	}
-	
+
 	@Test
 	public void ShouldReturnHostNameFromGivenUrl() throws MalformedURLException{
 		url = new Url(BASEURL, EXTENSION, false);
 		assertEquals("www.test.com", url.getHostName());
 	}
-	
+
 	@Test
 	public void ShouldReturnPortIfPortIsSpecifiedInUrl() throws MalformedURLException{
 		url = new Url("http://test:8080", EXTENSION, false);
 		assertEquals(8080, url.getPort());
 	}
-	
+
 	@Test
 	public void ShouldReturnDefaultPort80IfNoPortIsSpecifiedInUrl() throws MalformedURLException{
 		url = new Url("http://test", EXTENSION, false);
 		assertEquals(80, url.getPort());
 	}
-	
+
 }
