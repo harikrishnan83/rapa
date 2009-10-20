47dd3ee1df9347273f15cba6dda71dcc4a9c3ac1
Martin Mauch <mauch@crealytics.de>
2009-10-20 19:48:46 +0200

    Added .gitignore, cleaned DeleteResourceRestClientTest

--- test/org/rest/rapa/DeleteResourceRestClientTest.java
+++ test/org/rest/rapa/DeleteResourceRestClientTest.java
@@ -4,48 +4,39 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import java.io.IOException;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.rest.rapa.formatter.FormatHandler;
 import org.rest.rapa.resource.Resource;
 import org.rest.rapa.resource.ResourceImpl;
 
 public class DeleteResourceRestClientTest {
-	@Test
-	public void shouldDeleteResource() throws Exception{
-		Url resourceUrl = new Url("http://test.com", "xml", false);
-		Resource resource = new ResourceImpl(1);
-		FormatHandler handler = mock(FormatHandler.class);
-		HttpMethodExecutor httpMethodExecutor = mock(HttpMethodExecutor.class);
-		RestClient client = new RestClient(resourceUrl,handler,httpMethodExecutor);
-		assertThatRestClientDoesnotThrowExceptionOnDelete(resource, client);
+
+	private Url					resourceUrl;
+	private Resource			resource;
+	private FormatHandler		handler;
+	private HttpMethodExecutor	httpMethodExecutor;
+	private RestClient			client;
+
+	@Before
+	public void before() {
+		resourceUrl = new Url("http://test.com", "xml", false);
+		resource = new ResourceImpl(1);
+		handler = mock(FormatHandler.class);
+		httpMethodExecutor = mock(HttpMethodExecutor.class);
+		client = new RestClient(resourceUrl, handler, httpMethodExecutor);
 	}
-	
-	@Test
-	public void shouldFailToDeleteIfUnableToHttpDeleteResource() throws Exception{
-		Url resourceUrl = new Url("http://test.com", "xml", false);
-		Resource resource = new ResourceImpl(1);
-		FormatHandler formatHandler = mock(FormatHandler.class);
-		HttpMethodExecutor httpMethodExecutor = mock(HttpMethodExecutor.class);
-		doThrow(new IOException()).when(httpMethodExecutor).delete("http://test.com/1");
-		RestClient client = new RestClient(resourceUrl,formatHandler,httpMethodExecutor);
-		assertThatRestClientThrowsExceptionOnDelete(resource, client);
+
+	public void shouldDeleteResource() throws Exception {
+		client.delete(resource);
 	}
-	
-	
-	private void assertThatRestClientDoesnotThrowExceptionOnDelete(
-			Resource resource, RestClient client) {
-		try{
-			client.delete(resource);
-		}catch (RestClientException e) {
-			fail("Not supposed to throw a RestClientException");
-		}
-	}	
-	private void assertThatRestClientThrowsExceptionOnDelete(Resource resource,
-			RestClient client) {
-		try{
-			client.delete(resource);
-			fail("Supposed to throw a RestClientException");
-		}catch (RestClientException e) {
-		}
-	}	
+
+	@Test(expected = RestClientException.class)
+	public void shouldFailToDeleteIfUnableToHttpDeleteResource()
+			throws Exception {
+		doThrow(new IOException()).when(httpMethodExecutor).delete(
+				"http://test.com/1");
+		client.delete(resource);
+	}
 }
