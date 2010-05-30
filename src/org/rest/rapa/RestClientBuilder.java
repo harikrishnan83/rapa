3cc560f26da8a158bd6d092a3f39bb2156e7a5a5
HAriKrishnan <harikrishnan83@gmail.com>
2010-05-30 14:38:50 +0530

    Removing FormatHandlerFactory. Format are now plugin style

--- src/org/rest/rapa/RestClientBuilder.java
+++ src/org/rest/rapa/RestClientBuilder.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.httpclient.auth.AuthPolicy;
-import org.rest.rapa.formatter.FormatHandlerFactory;
-import org.rest.rapa.formatter.Formats;
+import org.rest.rapa.formatter.FormatHandler;
+import org.rest.rapa.formatter.JAXB.XMLHandler;
 
 public class RestClientBuilder {
 	private String url;
@@ -14,78 +14,80 @@ public class RestClientBuilder {
 	private String username = "";
 	private String scheme = "";
 	private String realm = "";
-	private Formats format;
 	private boolean formatAsExtenstion;
 	private List<String> authenticationPrefefences = new ArrayList<String>();
-	
-	public RestClientBuilder withUrl(String url){
+	private FormatHandler formatHandler = new XMLHandler();
+
+	public RestClientBuilder withUrl(String url) {
 		this.url = url;
 		return this;
 	}
-	
-	public RestClientBuilder withUserName(String username){
+
+	public RestClientBuilder withUserName(String username) {
 		this.username = username;
 		return this;
 	}
-	
-	public RestClientBuilder withPassword(String password){
+
+	public RestClientBuilder withPassword(String password) {
 		this.password = password;
 		return this;
 	}
-	
-	public RestClientBuilder withScheme(String scheme){
+
+	public RestClientBuilder withScheme(String scheme) {
 		this.scheme = scheme;
 		return this;
 	}
-	
-	public RestClientBuilder withRealm(String realm){
+
+	public RestClientBuilder withRealm(String realm) {
 		this.realm = realm;
 		return this;
 	}
-	
-	public RestClientBuilder withFormat(Formats format){
-		this.format = format;
-		return this;
-	}
-	
+
 	public RestClientBuilder withBasicAuthentication() {
 		this.authenticationPrefefences.add(AuthPolicy.BASIC);
 		return this;
 	}
-	
+
 	public RestClientBuilder withDigestAuthentication() {
 		this.authenticationPrefefences.add(AuthPolicy.DIGEST);
 		return this;
 	}
-	
+
 	public RestClientBuilder withNTLMAuthentication() {
 		this.authenticationPrefefences.add(AuthPolicy.NTLM);
 		return this;
 	}
-		
-	public RestClientBuilder useFormatAsExtension(){
+
+	public RestClientBuilder withFormatHandler(FormatHandler formatHandler) {
+		this.formatHandler = formatHandler;
+		return this;
+	}
+
+	public RestClientBuilder useFormatAsExtension() {
 		this.formatAsExtenstion = true;
 		return this;
 	}
-	
-	public RestClient build() throws MalformedURLException{
-		FormatHandlerFactory formatHandlerFactory = new FormatHandlerFactory();
-		formatHandlerFactory.create(format);
+
+	public RestClient build() throws MalformedURLException {
 		HttpClientAdapterImpl httpClientAdapter = new HttpClientAdapterImpl(
-		username, password, host(), port(), realm, scheme, authenticationPrefefences);
+				username, password, host(), port(), realm, scheme,
+				authenticationPrefefences);
 		HttpMethodProvider httpMethodProvider = new HttpMethodProvider();
-		
-		HttpMethodExecutor httpMethodExecutor = new HttpMethodExecutor(httpClientAdapter, httpMethodProvider);
-		Url resourceUrl = new Url(url, format.toString(), formatAsExtenstion);
-		return new RestClient(resourceUrl,formatHandlerFactory.create(format),httpMethodExecutor);
+
+		HttpMethodExecutor httpMethodExecutor = new HttpMethodExecutor(
+				httpClientAdapter, httpMethodProvider);
+		Url resourceUrl = new Url(url, formatHandler.getExtension(),
+				formatAsExtenstion);
+
+		return new RestClient(resourceUrl, formatHandler, httpMethodExecutor);
 	}
-	
-	private String host() throws MalformedURLException{
+
+	private String host() throws MalformedURLException {
 		return new Url(url, "", false).getHostName();
 	}
-	
-	private int port() throws MalformedURLException{
+
+	private int port() throws MalformedURLException {
 		return new Url(url, "", false).getPort();
 	}
-	
+
 }
